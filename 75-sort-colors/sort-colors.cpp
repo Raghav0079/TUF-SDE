61f2cefac7afb4c3364a8a0636dabf1520e6bf57
Raghav Mishra <raghav051006@gmail.com>
2026-06-09 10:56:04 +0530

    Time: 0 ms (100.00%) | Memory: 11.6 MB (77.62%) - LeetSync

---
+++ 75-sort-colors/sort-colors.cpp
@@ -0,0 +1,26 @@
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+    void sortColors(vector<int>& nums) { // LeetCode uses 'sortColors' as the function name
+        int low = 0, mid = 0, high = nums.size() - 1;
+
+        while (mid <= high) {
+            if (nums[mid] == 0) {
+                swap(nums[mid], nums[low]);
+                mid++;
+                low++;
+            }
+            else if (nums[mid] == 1) {
+                mid++;
+            }
+            else { // nums[mid] == 2
+                swap(nums[mid], nums[high]);
+                high--;
+            }
+        }
+    }
+};
