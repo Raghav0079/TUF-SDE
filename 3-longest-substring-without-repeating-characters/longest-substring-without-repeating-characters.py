4ab65edb187065e129ad06bb1d1e758edc279954
Raghav Mishra <raghav051006@gmail.com>
2026-06-12 15:27:32 +0530

    Time: 35 ms (21.72%) | Memory: 12.6 MB (80.49%) - LeetSync

---
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
@@ -0,0 +1,41 @@
+class Solution(object):
+    def lengthOfLongestSubstring(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        n = len(s)
+
+        HashLen = 256 
+
+        hash = [-1] * HashLen
+        for i in range(HashLen):
+            hash[i] = -1
+
+        l, r, maxLen = 0, 0, 0
+        while r < n:
+            """ If current character s[r] 
+                is already in the substring """
+            if hash[ord(s[r])] != -1:
+                """ Move left pointer to the right
+                    of the last occurrence of s[r] """
+                l = max(hash[ord(s[r])] + 1, l)
+            
+            # Calculate the current substring length
+            current_len = r - l + 1
+            
+            # Update maximum length found so far
+            maxLen = max(current_len, maxLen)
+            
+            """ Store the index of the current
+                character in the hash table """
+            hash[ord(s[r])] = r
+            
+            # Move right pointer to next position
+            r += 1
+        
+        # Return the maximum length found
+        return maxLen
+
+
+        
